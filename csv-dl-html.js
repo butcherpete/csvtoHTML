623a7e913c1f2288802a9575fd62baf266668be4
butcherpete <magpie.picaraza@gmail.com>
2022-02-25 16:54:35 -0800

    Update with input paths

--- csv-dl-html.js
+++ csv-dl-html.js
@@ -1,15 +1,30 @@
 const fs = require('fs');
 const Papa = require("papaparse");
+const { argv } = require('process');
+
+//let inputPath;
+//let outputPath;
+
+/* if (process.argv.length !=4) {
+    console.log("Pass two parameters: csv-input html-output");
+    process.exit(1);
+}
+    else {
+        inputPath = process.arg[3];
+        outputPath = process.arg[4];
+    }
+ */
+// Input variables
+//const options = {/* options */}; papaparse option ignore first line
+//const css-class = CSS class of definition list
 const inputPath = 'shortcsv.csv';
+const outputPath = 'html/dl-output.txt';
 
-//const options = {/* options */};
-
-console.log("start the stream")
 const dataStream = fs.createReadStream(inputPath)
 const parseStream = Papa.parse(Papa.NODE_STREAM_INPUT);
-
 dataStream.pipe(parseStream);
 
+//let writeStream = fs.createWriteStream("html/dl-output.txt");
 let data = [];
 let output = "<dl class=\"myclass\">\n"; 
 parseStream.on("data", chunk => {
@@ -19,11 +34,20 @@ parseStream.on("data", chunk => {
         output += ` <dt>${k}</dt>\n <dd>${v}</dd>\n`;
     }
     output = output += "</dl>";
+    //writeStream.write(output)
 });
 
 parseStream.on("finish", () => {
     //console.log(data);
     console.log(data.length);
     console.log(output);
-    fs.createWriteStream("html/dl-output.txt", output);
+    console.log(argv);
+    return output;
 });
+
+
+ 
+fs.writeFile(outputPath, output, function(err,result) {
+    if(err) console.log('error',err);
+    } 
+);
