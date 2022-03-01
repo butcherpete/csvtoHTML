a88d85e3a11ca3173d158de9d8c8f02cdfb47e0a
butcherpete <magpie.picaraza@gmail.com>
2022-03-01 01:10:55 -0800

    Working html converter

--- index.js
+++ index.js
@@ -1,46 +1,87 @@
 const fs = require('fs')
 const csv = require('csv-parser')
-const randomWords = require('random-words')
-const users = [];
-function generateUsername(firstname, surname) {
+const pretty = require('pretty');
+
+const inputPath = 'job-types.csv'
+const outputPath = 'html/job-types.htm';
+
+
+//let writeStream = fs.createWriteStream(outputPath);
+/* const tableStart = [];
+tableStart.push(tableClass);
+
+ */
+const tableRows = [];
+const tableHeaders =  [];
+
+/* function generateUsername(firstname, surname) {
     return `${firstname[0]}-${surname}`.toLowerCase();
 }
-fs.createReadStream('input.csv')
-  .pipe(csv())
-  .on('data', function (row) {
-    const username = generateUsername(row.Firstname, row.Surname);
-    const password = randomWords(3).join("-");
-    
-    const user = {
-        username,
-        firstname: row.Firstname,
-        surname: row.Surname,
-        roles: row.Roles,
-        password
-    }
-    users.push(user)
-  })
-  .on('end', function () {
-      console.table(users)
-      writeToCSVFile(users)
+ */
+
+fs.createReadStream(inputPath)
+    .pipe(csv())
+    .on('error', () => {
+        // handle error 
     })
 
-    function writeToCSVFile(users) {
-        const filename = 'output.csv';
-        fs.writeFile(filename, extractAsCSV(users), err => {
+    .on('headers',(headers) =>{
+
+        //console.log(`First header: ${headers[0]} and Second header: ${headers[1]}`)
+        let header = `<tr><th> ${headers[0]} </th><th> ${headers[1]}</th></tr>\n`;
+        tableHeaders.push(header);
+      
+    })
+
+    .on('data', function (row) {
+        let rows = `<tr><td> ${row["ID"]} </td><td> ${row["Definition"]}</td></tr>\n`;
+        tableRows.push(rows);
+        
+    })
+
+    .on('end', function () {
+        //console.log(tableRows);
+        //console.log(tableHeaders);
+        const tableStart = '<table>';
+        const tableEnd = '</table>';
+        //tableHeaders.toString;
+        //tableRows.toString;
+
+        //let tableRows = JSON.stringify(null, '\n');
+        let htmlList = tableStart.concat(tableHeaders, tableRows.join(""), tableEnd);
+
+        htmlList = JSON.stringify(htmlList, null, "");
+        let html = new String();
+        html = htmlList.toString().replace(/\"/g, "");
+        html = pretty(html, {ocd: true});
+        //html = html.split('\n');
+ 
+
+        //html = html.replace(/\"/g, "");
+       //console.log(pretty(html, {ocd: true}));
+       writeToCSVFile(html)
+    })
+
+    function writeToCSVFile(html) {
+        const filename = 'output.txt';
+
+        //const tableEnd = ['</table>']
+
+        fs.writeFile(filename, html, err => {
           if (err) {
             console.log('Error writing to csv file', err);
           } else {
             console.log(`saved as ${filename}`);
+            console.log(html);
           }
         });
       }
       
-      function extractAsCSV(users) {
+/*       function extractAsCSV(users) {
         const header = ["Username, Password, Roles"];
         const rows = users.map(user =>
            `${user.username}, ${user.password}, ${user.roles}`
         );
         return header.concat(rows).join("\n");
       }
-      
+ */      
